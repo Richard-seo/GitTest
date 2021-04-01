172beca6a5017cec2d1853f714ce4a966ab6c77c
HNTVDI\HF972 <seoky@hanatour.com>
2021-04-01 14:44:25 +0900

    second commit

--- src/Test/Test2.java
+++ src/Test/Test2.java
@@ -1,11 +1,7 @@
 package Test;
 
-public class Java_Test {
+public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("test");
-	}
-	
-	public void test() {
 		System.out.println("test");
 	}
 }
