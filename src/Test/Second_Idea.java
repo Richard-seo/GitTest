00898e791061771e1a6538aa19073c1bfd1d987a
HNTVDI\HF972 <seoky@hanatour.com>
2021-04-01 15:12:23 +0900

    second idea first commit

---
+++ src/Test/Second_Idea.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Second_Idea {
+	public Second_Idea() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("second idea");
+	}
+}
