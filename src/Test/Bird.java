87cd7f53affa682521c4612a388eb11da3113b4b
HNTVDI\HF972 <seoky@hanatour.com>
2021-04-01 15:48:46 +0900

    github second commit

--- src/Test/Bird.java
+++ src/Test/Bird.java
@@ -1,11 +1,11 @@
 package Test;
 
-public class Second_Idea {
-	public Second_Idea() {
+public class Bird {
+	public Bird() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("third idea");
+		System.out.println("bird");
 	}
 }
--- src/Test/Dog.java
+++
@@ -1,7 +0,0 @@
-package Test;
-
-public class Dog {
-	public static void main(String[] args) {
-		System.out.println("dog");
-	}
-}
